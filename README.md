9e9c9503a0d07d723ddd0b7321a008f42e857e1b
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-06-16 09:45:30 +0500

    updated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Language Translation 
- Task 1 at CodeAlpha 
+# Language Translation
+
+## Task 1 at CodeAlpha
+
+This project is a language translation application built using Streamlit and the Google Translate API (`googletrans` library).
+
+### Features
+
+- Translate text between different languages using Google Translate API.
+- Select source and target languages from dropdown menus.
+- Real-time translation display with error handling.
+
+### Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/Raufjatoi/Language-Translation.git
+   cd Language-Translation
+
+2. Install dependencies:
+    ```bash
+    pip install -r requirements.txt
+
+3. run app:
+    ```bash
+    streamlit run app.py
---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from googletrans import Translator, LANGUAGES
+
+# Translator instance
+translator = Translator()
+
+# Streamlit App
+def main():
+    st.title('Language Translator')
+
+    # Input text box for user input
+    text = st.text_area('Enter text to translate')
+
+    # Selectbox for choosing source language
+    source_lang = st.selectbox('Select source language', ['auto'] + list(LANGUAGES.keys()))
+
+    # Selectbox for choosing target language
+    target_lang = st.selectbox('Select target language', list(LANGUAGES.keys()))
+
+    if st.button('Translate'):
+        if text.strip() != '':
+            try:
+                # Translate text
+                translated_text = translator.translate(text, src=source_lang, dest=target_lang).text
+                st.success(f"Translated Text ({target_lang}):")
+                st.write(translated_text)
+            except Exception as e:
+                st.error(f"Translation error: {e}")
+        else:
+            st.warning("Please enter some text to translate.")
+
+if __name__ == '__main__':
+    main()
